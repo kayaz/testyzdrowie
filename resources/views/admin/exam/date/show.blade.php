6ec5f90a4c18385f7e2f0f2435232dd03d349af6
Jacek <kontakt@jk-design.pl>
2023-06-12 11:51:44 +0200

    Open .pdf files in modal

--- resources/views/admin/exam/date/show.blade.php
+++ resources/views/admin/exam/date/show.blade.php
@@ -5,7 +5,7 @@
         <div class="card-head container-fluid">
             <div class="row">
                 <div class="col-6 pl-0">
-                    <h4 class="page-title"><i class="fe-book-open"></i> - Termin: {{ $examdate->start }} <i class="las la-long-arrow-alt-right me-2 ms-2"></i> {{ $examdate->end }}</h4>
+                    <h4 class="page-title"><i class="fe-book-open"></i>{{ $exam->name }} - Termin: {{ $examdate->start }} <i class="las la-long-arrow-alt-right me-2 ms-2"></i> {{ $examdate->end }}</h4>
                 </div>
                 <div class="col-6 d-flex justify-content-end align-items-center form-group-submit">
 
@@ -41,7 +41,7 @@
                         <tbody class="content">
                             @foreach($examdateusers as $item)
                                 <tr>
-                                    <td>@if($item->users->first()->name) {{ $item->users->first()->name }} @endif</td>
+                                    <td></td>
                                     <td>@if($item->users->first()->surname) {{ $item->users->first()->surname }} @endif</td>
                                     <td class="text-center">{{ $item->users->first()->specialization }}</td>
                                     <td class="text-center">{!! checkPwz($item->users->first()->practice) !!}</td>
