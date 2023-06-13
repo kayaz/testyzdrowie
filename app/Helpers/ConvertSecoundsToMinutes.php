bb54b102e21317d74a39708a7e7bfba2fdf821b6
Jacek <kontakt@jk-design.pl>
2023-06-13 17:02:39 +0200

    Open .pdf files in modal

--- app/Helpers/ConvertSecoundsToMinutes.php
+++ app/Helpers/ConvertSecoundsToMinutes.php
@@ -1,8 +1,12 @@
 <?php
 use Carbon\Carbon;
 if (! function_exists('convertSec2Min')) {
-    function convertSec2Min(int $seconds): string
+    function convertSec2Min(?int $seconds): string
     {
+        if ($seconds === null) {
+            $seconds = 0;
+        }
+
         $minutes = floor($seconds / 60);
         $remainingSeconds = $seconds % 60;
         $time = Carbon::createFromTime(0, $minutes, $remainingSeconds);
